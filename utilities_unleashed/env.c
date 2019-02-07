9fb1903497b59cb68c96f838d614fd1d201d9cc5
Jialong Yin <jialong2@illinois.edu>
2019-02-06 22:27:03 -0600

    My Submission

--- utilities_unleashed/env.c
+++ utilities_unleashed/env.c
@@ -2,7 +2,72 @@
  * Utilities Unleashed Lab
  * CS 241 - Spring 2019
  */
- 
+ #include <stdio.h>
+ #include <stdint.h>
+ #include <unistd.h>
+ #include <stdlib.h>
+ #include <sys/types.h>
+ #include <sys/wait.h>
+ #include <string.h>
+ #include <ctype.h>
+ #include "format.h"
+
 int main(int argc, char *argv[]) {
-    return 0;
+  if (argc < 2) {
+    print_env_usage();
+  }
+
+  pid_t pid = fork();
+  if (pid < 0) { // fork failure
+      print_fork_failed();
+  } else if (pid > 0) { // I am the parent
+      int status;
+      waitpid(pid, &status, 0);
+      return 0;
+  } else {
+        int i = 1;
+        while ( argv[i] ) {
+          // check env or exec
+          if (strcmp(argv[i], "--")) {
+                char *key = strtok(argv[i], "=");
+                char *value = strtok(NULL, "");
+                if (value == NULL) print_env_usage();
+                // check key value are only number letter underscore
+                char *ptr_k = key;
+                while (*ptr_k) {
+                    if ( !(isalpha(*ptr_k) || isdigit(*ptr_k) || *ptr_k == '_') ) {
+                        print_env_usage();
+                    }
+                    ptr_k++;
+                }
+                // check value or reference
+                if (value[0] != '%') {
+                    char *ptr_v = value;
+                    while (*ptr_v) {
+                        if ( !(isalpha(*ptr_v) || isdigit(*ptr_v) || *ptr_v == '_') ) {
+                            print_env_usage();
+                        }
+                        ptr_v++;
+                    }
+                } else {
+                    // printf("ref.: %s\n", value+1);
+                    value = getenv(value+1);
+                    // printf("ref. value: %s\n", value);
+                    if (!value) {
+                        print_environment_change_failed();
+                    }
+                }
+                int flag = setenv(key, value, 1);
+                if (flag < 0)
+                    print_environment_change_failed();
+          } else {
+                    // printf("argv[i+1]: %s\n", argv[i+1]);
+                    execvp(argv[i+1], argv+i+1);
+                    print_exec_failed();
+          }
+          i++;
+        }
+        // print_env_usage();
+  }
+  return 0;
 }
--- utilities_unleashed/time.c
+++ utilities_unleashed/time.c
@@ -2,8 +2,43 @@
  * Utilities Unleashed Lab
  * CS 241 - Spring 2019
  */
- 
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include "format.h"
+
+#define BILLION  1000000000L
 
 int main(int argc, char *argv[]) {
-    return 0;
+  if (argc < 2) {
+    print_time_usage();
+  }
+
+  pid_t pid = fork();
+  if (pid < 0) { // fork failure
+    print_fork_failed();
+  } else if (pid > 0) { // I am the parent
+    struct timespec start, end;
+    double duration;
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
+    int status;
+    pid_t result_pid = waitpid(pid, &status, 0);
+
+    clock_gettime(CLOCK_MONOTONIC, &end);
+
+    if (result_pid != -1 && WIFEXITED(status)) {
+      duration = (end.tv_sec - start.tv_sec) + (double) (end.tv_nsec - start.tv_nsec) / (double) BILLION;
+      display_results(argv, duration);
+    }
+  } else { // I am the child
+    execvp(argv[1], argv+1);
+    print_exec_failed();
+  }
+
+  return 0;
 }
