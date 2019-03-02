639dd3160674b72c2fbe5d65cf95ec2aef65c64a
Jialong Yin <jialong2@illinois.edu>
2019-03-01 18:36:32 -0600

    My Submission

--- deadlock_demolition/libdrm.c
+++ deadlock_demolition/libdrm.c
@@ -2,30 +2,91 @@
  * Deadlock Demolition Lab
  * CS 241 - Spring 2019
  */
- 
+
 #include "graph.h"
 #include "libdrm.h"
 #include "set.h"
 #include <pthread.h>
 
-struct drm_t {};
-
+struct drm_t {pthread_mutex_t m;};
+graph *g = NULL;
+set *visited = NULL;
+pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+bool isCyclic(void* node) {
+    if (!visited) {
+      visited = shallow_set_create();
+    }
+    if (set_contains(visited, node)) {
+        visited = NULL;
+        return true;
+    } else {
+      set_add(visited, node);
+      vector *neighbors = graph_neighbors(g, node);
+      for (size_t i = 0; i < vector_size(neighbors); i++) {
+        if (isCyclic(vector_get(neighbors, i))) {
+          return true;
+        }
+      }
+    }
+    visited = NULL;
+    return false;
+}
 drm_t *drm_init() {
     /* Your code here */
-    return NULL;
+    drm_t *drm = malloc(sizeof(drm_t));
+    pthread_mutex_init(&drm->m, NULL);
+
+    pthread_mutex_lock(&m);
+    if (!g) {
+      g = shallow_graph_create();
+    }
+    graph_add_vertex(g, drm);
+    pthread_mutex_unlock(&m);
+
+    return drm;
 }
 
 int drm_post(drm_t *drm, pthread_t *thread_id) {
     /* Your code here */
-    return 0;
+    if (pthread_mutex_unlock(&drm->m)) return 0;
+
+    pthread_mutex_lock(&m);
+    graph_remove_edge(g, drm, thread_id);
+    pthread_mutex_unlock(&m);
+
+    return 1;
 }
 
 int drm_wait(drm_t *drm, pthread_t *thread_id) {
     /* Your code here */
-    return 0;
+
+    pthread_mutex_lock(&m);
+    graph_add_vertex(g, thread_id);
+    graph_add_edge(g, thread_id, drm);
+    if (isCyclic(thread_id)) {
+      graph_remove_edge(g, thread_id, drm);
+      pthread_mutex_unlock(&m);
+
+      return 0;
+    } else {
+      graph_remove_edge(g, thread_id, drm);
+      graph_add_edge(g, drm, thread_id);
+      pthread_mutex_unlock(&m);
+
+      pthread_mutex_lock(&drm->m);
+
+      return 1;
+    }
 }
 
 void drm_destroy(drm_t *drm) {
     /* Your code here */
+
+    pthread_mutex_lock(&m);
+    graph_remove_vertex(g, drm);
+    pthread_mutex_unlock(&m);
+
+    pthread_mutex_destroy(&drm->m);
+    free(drm);
     return;
 }
--- deadlock_demolition/libdrm_tester.c
+++ deadlock_demolition/libdrm_tester.c
@@ -2,7 +2,7 @@
  * Deadlock Demolition Lab
  * CS 241 - Spring 2019
  */
- 
+
 #include "libdrm.h"
 
 #include <assert.h>
@@ -10,11 +10,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+int test1 = 0;
+int test2 = 0;
+drm_t *drm1 = NULL;
+drm_t *drm2 = NULL;
+size_t num_threads = 2;
 
+void *myfunc1(void *tid) {
+  if (drm_wait(drm1, tid))
+    test1++;
+  else printf("deadlock!!!\n");
+  printf("1 first\n");
+  if (drm_wait(drm2, tid)) {
+    for (int i = 0; i < 10000; i++) {
+      test2++;
+    }
+  } else printf("deadlock!!!\n");
+
+  if (drm_post(drm2, tid) == 0)
+    printf("error!!!\n");
+
+  if (drm_post(drm1, tid) == 0)
+    printf("error!!!\n");
+  return NULL;
+}
+void *myfunc2(void *tid) {
+  printf("2 first\n");
+  if (drm_wait(drm2, tid)) {
+    for (int i = 0; i < 1000000; i++) {
+      test2++;
+    }
+  } else printf("deadlock!!!\n");
+  
+  if (drm_wait(drm1, tid))
+    test1++;
+  else printf("deadlock!!!\n");
+
+  if (drm_post(drm1, tid) == 0)
+    printf("error!!!\n");
+
+  if (drm_post(drm2, tid) == 0)
+    printf("error!!!\n");
+
+  return NULL;
+}
 int main() {
-    drm_t *drm = drm_init();
-    // TODO your tests here
-    drm_destroy(drm);
+    drm1 = drm_init();
+    drm2 = drm_init();
+    pthread_t tid[num_threads];
+    for (size_t i = 0; i < num_threads; i++) {
+      if (i==0) {
+        pthread_create(tid+i, NULL, myfunc1, tid);
+      } else {
+        pthread_create(tid+i, NULL, myfunc2, tid+1);
+      }
 
+      if (i == num_threads-1) {
+        for (size_t j = 0; j < num_threads; j++) {
+          pthread_join(tid[j], NULL);
+        }
+      }
+    }
+    printf("test1: %d\n", test1);
+    printf("test2: %d\n", test2);
+    drm_destroy(drm1);
+    drm_destroy(drm2);
+    pthread_exit(NULL);
     return 0;
 }
