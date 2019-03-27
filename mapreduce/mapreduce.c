97dd6f35ad6931eb90342dbbd6a85def4474d736
Jialong Yin <jialong2@illinois.edu>
2019-03-26 23:47:00 -0500

    My Submission

--- mapreduce/mapreduce.c
+++ mapreduce/mapreduce.c
@@ -2,27 +2,147 @@
  * mapreduce Lab
  * CS 241 - Spring 2019
  */
- 
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 #include "utils.h"
 
+#define SPLITTER "./splitter"
+
+/* pointer to fds for mapper  */
+int* mappers_fds;
+/* fds for reducer */
+int reducer_fds[2];
+
 int main(int argc, char **argv) {
+    // check usage
+    // ./mapreduce <input_file> <output_file> <mapper_executable> <reducer_executable> <mapper_count>
+    if (argc != 6) {
+      print_usage();
+      exit(1);
+    }
+    // parse args
+    char* input_file = argv[1];
+    char* output_file = argv[2];
+    char* mapper_name = argv[3];
+    char* reducer_name = argv[4];
+    size_t mapper_count = (size_t)atoi(argv[5]);
+
+    // save child pid
+    pid_t splitter_pids[128];
+    pid_t mappers_pids[128];
+    pid_t reducer_pid;
+
     // Create an input pipe for each mapper.
+    mappers_fds = (int*)malloc(sizeof(int) * mapper_count * 2);
+    size_t i = 0;
+    for (; i < mapper_count; i++) {
+      pipe(&mappers_fds[2 * i]);
+    }
 
     // Create one input pipe for the reducer.
+    pipe(reducer_fds);
 
     // Open the output file.
+    // open(pathname, flags, mode)
+    // mode must be supplied when O_CREAT or O_TMPFILE is specified in flags
+    int output_fd = open(output_file, O_CREAT | O_TRUNC | O_WRONLY, S_IRWXU | S_IRWXG | S_IRWXO);
 
     // Start a splitter process for each mapper.
+    for (i = 0; i < mapper_count; i++) {
+      splitter_pids[i] = fork();
+      if (splitter_pids[i] == 0) {  // child : splitter
+        size_t readend = 2*i;
+        size_t writend = 2*i + 1;
+        close(mappers_fds[readend]);   // close write end for reducer
+        dup2(mappers_fds[writend], 1);      // redirect output file des to stdout
+
+        char str_mapper_count[5];
+        sprintf(str_mapper_count, "%zu", mapper_count);
+        char str_i[5];
+        sprintf(str_i, "%zu", i);
+        int execret = execl(SPLITTER, SPLITTER, input_file, str_mapper_count, str_i, NULL);
+        # ifdef DEBUG
+        perror("");
+        # endif
+        return execret;
+      } else { // parent
+        // just continue
+      }
+    }
+
+    for (i = 0; i < mapper_count; i++) {
+       // close write end for every mapper --> not blocking mappers
+      size_t writend = 2*i + 1;
+      close(mappers_fds[writend]);
+    }
 
     // Start all the mapper processes.
+    for (i = 0; i < mapper_count; i++) {
+      mappers_pids[i] = fork();
+      if (mappers_pids[i] == 0) {  // child : mapper
+        size_t readend = 2*i;
+        size_t writend = 2*i + 1;
+        close(mappers_fds[writend]);   // close write end for reducer
+        close(reducer_fds[0]);
+        dup2(mappers_fds[readend], 0);      // redirect output file des to stdout
+        dup2(reducer_fds[1], 1); // redirect reducer writeend to stdout
+
+        int execret = execl(mapper_name, mapper_name, NULL);
+        return execret;
+      } else { // parent
+        // just continue
+      }
+    }
 
     // Start the reducer process.
+    reducer_pid = fork();
+    if (reducer_pid== 0) { // child!
+      close(reducer_fds[1]);   // close write end for reducer
+      dup2(reducer_fds[0], 0); // redirect reducer pipe read-end to stdin
+      dup2(output_fd, 1);      // redirect output file des to stdout
 
-    // Wait for the reducer to finish.
+      int execret = execl(reducer_name, reducer_name, NULL);
+      return execret;
+    } else { // parent
+      // Wait for the reducer to finish.
+      close(reducer_fds[0]);
+      close(reducer_fds[1]);
+    }
 
     // Print nonzero subprocess exit codes.
+    // wait for all splitter
+    int status;
+    for (i = 0; i < mapper_count; i++) {
+      waitpid(splitter_pids[i], &status, 0);
+      if (status) {
+        print_nonzero_exit_status(SPLITTER, status);
+      }
+    }
+
+    // wait for all mappers
+    for (i = 0; i < mapper_count; i++) {
+      waitpid(mappers_pids[i], &status, 0);
+      if (status) {
+        print_nonzero_exit_status(mapper_name, status);
+      }
+    }
+
+    waitpid(reducer_pid, &status, 0);
+    if (status) {
+      print_nonzero_exit_status(reducer_name, status);
+    }
 
     // Count the number of lines in the output file.
+    print_num_lines(output_file);
 
+    free(mappers_fds);
     return 0;
 }
--- parallel_make/parmake.c
+++ parallel_make/parmake.c
@@ -17,75 +17,130 @@
 #include <time.h>
 graph *g = NULL;
 set *visited = NULL;
+queue *q = NULL;
+int failed = 0;
+pthread_cond_t cv;
+pthread_mutex_t m;
 
 bool isCyclic(char* target) {
     if (!visited) {
       visited = shallow_set_create();
     }
-    int flag_run = 0;
     if (set_contains(visited, target)) {
-        // Part2: edit global var.: sum, failed
         set_destroy(visited);
         visited = NULL;
         return true;
     } else {
       set_add(visited, target);
-      if (access(target, F_OK) < 0) {
-        flag_run = 1;
-      }
       vector *neighbors = graph_neighbors(g, target);
       for (size_t i = 0; i < vector_size(neighbors); i++) {
-        char *neighbor = vector_get(neighbors, i);
-        if (!flag_run && access(neighbor, F_OK) < 0) {
-          flag_run = 1;
-        } else if (!flag_run && access(target, F_OK) == 0 && access(neighbor, F_OK) == 0) {
-          struct stat trg;
-          stat(target, &trg);
-          struct stat nbr;
-          stat(neighbor, &nbr);
-          if (difftime(trg.st_mtime, nbr.st_mtime) < 0) {
-            flag_run = 1;
-          }
-        }
-        // Part2: check status only after iteration
-        if (isCyclic(neighbor)) {
+        if (isCyclic(vector_get(neighbors, i))) {
           vector_destroy(neighbors);
           return true;
         }
       }
-      // Part2: if failed return true; wait here until sum = size(neighbors) 
       vector_destroy(neighbors);
-    }
-    // Part2: put all below in threads -- global var.: sum, failed
-    rule_t *rule = (rule_t *) graph_get_vertex_value(g, target);
-    if(!(rule->state) && flag_run) {
-      rule->state = 1;
-      for (size_t i = 0; i < vector_size(rule->commands); i++) {
-        if (system(vector_get(rule->commands, i)) != 0) {
-          set_destroy(visited);
-          visited = NULL;
-          return true;
-        }
-      }
     }
     set_destroy(visited);
     visited = NULL;
     return false;
 }
+//
+bool all_satisfied(vector *neighbors) {
+  for (size_t i = 0; i < vector_size(neighbors); i++) {
+    char *neighbor = vector_get(neighbors, i);
+    rule_t *rule_nbr = (rule_t *) graph_get_vertex_value(g, neighbor);
+    if (!(rule_nbr->state)) return false;
+  }
+  return true;
+}
+//
+bool isFailed(char* target) {
+    int flag_run = 0;
+    if (access(target, F_OK) < 0) {
+      flag_run = 1;
+    }
+    vector *neighbors = graph_neighbors(g, target);
+    for (size_t i = 0; i < vector_size(neighbors); i++) {
+      char *neighbor = vector_get(neighbors, i);
+      if (!flag_run && access(neighbor, F_OK) < 0) {
+        flag_run = 1;
+      } else if (!flag_run && access(target, F_OK) == 0 && access(neighbor, F_OK) == 0) {
+        struct stat trg;
+        stat(target, &trg);
+        struct stat nbr;
+        stat(neighbor, &nbr);
+        if (difftime(trg.st_mtime, nbr.st_mtime) < 0) {
+          flag_run = 1;
+        }
+      }
+      if (isFailed(neighbor))
+          return true;
+    }
+    if (vector_size(neighbors)) {
+      pthread_mutex_lock(&m);
+      while (!all_satisfied(neighbors)) {
+        pthread_cond_wait(&cv, &m);
+      }
+      pthread_mutex_unlock(&m);
+    }
+    vector_destroy(neighbors);
+    if (failed) return true;
+    rule_t *rule = (rule_t *) graph_get_vertex_value(g, target);
+    if (flag_run && !(rule->state)) {
+      queue_push(q, rule);
+    } else {
+      rule->state = 1;
+    }
+    return false;
+}
+void *myfunc(void *ptr) {
+  while (1) {
+    rule_t *rule = queue_pull(q);
+    if (!rule) {
+      queue_push(q, NULL);
+      break;
+    }
+    for (size_t i = 0; i < vector_size(rule->commands); i++) {
+      if (system(vector_get(rule->commands, i)) != 0) {
+        failed = 1;
+        break;
+      }
+    }
+    rule->state = 1;
+    pthread_cond_signal(&cv);
+  }
+  return NULL;
+}
 int parmake(char *makefile, size_t num_threads, char **targets) {
     // good luck!
+    q = queue_create(-1);
+    pthread_t tid[num_threads];
+    for (size_t i = 0; i < num_threads; i++) {
+      pthread_create(tid+i, NULL, myfunc, NULL);
+    }
+
+    pthread_cond_init(&cv, NULL);
+    pthread_mutex_init(&m, NULL);
     g = parser_parse_makefile(makefile, targets);
-    // printf("pass here\n");
-    vector *goals;
-    goals = graph_neighbors(g, "");
+    vector *goals = graph_neighbors(g, "");
     for (size_t i = 0; i < vector_size(goals); i++) {
       char *goal = vector_get(goals, i);
       if (isCyclic(goal)) {
         print_cycle_failure(goal);
-        // Part2: edit global var.: failed = 0
+      } else {
+        if (isFailed(goal))
+          failed = 0;
       }
+    }
+    queue_push(q, NULL);
+    for (size_t j = 0; j < num_threads; j++) {
+      pthread_join(tid[j], NULL);
     }
     vector_destroy(goals);
     graph_destroy(g);
+    queue_destroy(q);
+    pthread_mutex_destroy(&m);
+    pthread_cond_destroy(&cv);
     return 0;
 }
