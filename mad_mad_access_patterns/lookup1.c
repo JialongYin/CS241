0593cb4393dcd182cd33cde07ce953b2c037e75f
Jialong Yin <jialong2@illinois.edu>
2019-04-10 15:33:47 -0500

    My Submission

--- mad_mad_access_patterns/lookup1.c
+++ mad_mad_access_patterns/lookup1.c
@@ -4,13 +4,60 @@
  */
 #include "tree.h"
 #include "utils.h"
+#include <stdlib.h>
+#include <string.h>
 /*
   Look up a few nodes in the tree and print the info they contain.
   This version uses fseek() and fread() to access the data.
 
   ./lookup1 <data_file> <word> [<word> ...]
 */
-
+BinaryTreeNode *wordBinarySearch(uint32_t offset, FILE *file, char *word) {
+  if (offset == 0) return NULL;
+  fseek(file, offset, SEEK_SET);
+  BinaryTreeNode *node = calloc(1,sizeof(BinaryTreeNode)+10);
+  fread(node, sizeof(BinaryTreeNode)+10, 1, file);
+  if (strcmp(word, node->word) == 0) {
+    return node;
+  }
+  BinaryTreeNode *word_node;
+  if ((word_node = wordBinarySearch(node->left_child, file, word))) {
+    free(node);
+    return word_node;
+  }
+  if ((word_node = wordBinarySearch(node->right_child, file, word))) {
+    free(node);
+    return word_node;
+  }
+  free(node);
+  return NULL;
+}
 int main(int argc, char **argv) {
-    return 0;
+  if (argc < 3) {
+    printArgumentUsage();
+    exit(1);
+  }
+  char *file_name = argv[1];
+  FILE *file = fopen(file_name, "r");
+  if (file == 0) {
+    openFail(file_name);
+    exit(2);
+  }
+  char root[BINTREE_ROOT_NODE_OFFSET];
+  fread(root, 1, BINTREE_ROOT_NODE_OFFSET, file);
+  if (strcmp(root, BINTREE_HEADER_STRING)) {
+    formatFail(file_name);
+    exit(2);
+  }
+  for (int i = 2; i < argc; i++) {
+    BinaryTreeNode *word_node = wordBinarySearch(BINTREE_ROOT_NODE_OFFSET, file, argv[i]);
+    if (word_node) {
+      printFound(word_node->word, word_node->count, word_node->price);
+      free(word_node);
+    } else {
+      printNotFound(argv[i]);
+    }
+  }
+  fclose(file);
+  return 0;
 }
--- mad_mad_access_patterns/lookup2.c
+++ mad_mad_access_patterns/lookup2.c
@@ -4,14 +4,67 @@
  */
 #include "tree.h"
 #include "utils.h"
-
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
 /*
   Look up a few nodes in the tree and print the info they contain.
   This version uses mmap to access the data.
 
   ./lookup2 <data_file> <word> [<word> ...]
 */
-
+BinaryTreeNode *wordBinarySearch(uint32_t offset, char *addr, char *word) {
+  if (offset == 0) return NULL;
+  BinaryTreeNode *node = (BinaryTreeNode *) (addr+offset);
+  if (strcmp(word, node->word) == 0) {
+    return node;
+  }
+  BinaryTreeNode *word_node;
+  if ((word_node = wordBinarySearch(node->left_child, addr, word))) {
+    return word_node;
+  }
+  if ((word_node = wordBinarySearch(node->right_child, addr, word))) {
+    return word_node;
+  }
+  return NULL;
+}
 int main(int argc, char **argv) {
-    return 0;
+  if (argc < 3) {
+    printArgumentUsage();
+    exit(1);
+  }
+  char *file_name = argv[1];
+  int fd = open(file_name, O_RDONLY);
+  if (fd < 0) {
+    openFail(file_name);
+    exit(2);
+  }
+  struct stat filestat;
+  if(fstat(fd, &filestat) !=0) {
+    openFail(file_name);
+    exit(2);
+  }
+  char* addr = mmap(NULL, filestat.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+  if (addr == (void *)-1) {
+    mmapFail(file_name);
+    exit(2);
+  }
+  if (strncmp(addr, BINTREE_HEADER_STRING, BINTREE_ROOT_NODE_OFFSET)) {
+    formatFail(file_name);
+    exit(2);
+  }
+  for (int i = 2; i < argc; i++) {
+    BinaryTreeNode *word_node = wordBinarySearch(BINTREE_ROOT_NODE_OFFSET, addr, argv[i]);
+    if (word_node) {
+      printFound(word_node->word, word_node->count, word_node->price);
+    } else {
+      printNotFound(argv[i]);
+    }
+  }
+  close(fd);
+  return 0;
 }
